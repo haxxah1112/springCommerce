8b6cd8078b12a4b205e301ea600d59689a028647
Hanna Kim <hannakim@Hannaui-MacBookPro.local>
2024-11-16 00:31:54 +0900

    feat: JsonUtil 클래스 common 모듈로 이동

---
+++ common-module/src/main/java/com/project/util/JsonUtil.java
@@ -0,0 +1,24 @@
+package com.project.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static <T> String serialize(T object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to serialize object", e);
+        }
+    }
+
+    public static <T> T deserialize(String message, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(message, clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to deserialize message", e);
+        }
+    }
+}
